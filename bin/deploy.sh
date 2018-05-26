a7665130537d32a7b230903f0d70151ac63f722d
Travis CI <waldermort@gmail.com>
2018-05-26 18:15:43 +0800

    update

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -32,6 +32,7 @@ mv dist/php-interpreter.phar dist/php-interpreter.phar.tmp
 
 # Checkout gh-pages and add PHAR file and version:
 git checkout gh-pages
+ls -al dist
 mv dist/php-interpreter.tmp dist/php-interpreter.phar
 sha1sum dist/php-interpreter.phar > dist/php-interpreter.phar.version
 git add dist/php-interpreter.phar dist/php-interpreter.phar.version
